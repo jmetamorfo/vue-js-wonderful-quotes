f84470531b83e91238fb4386d5256588bcd70531
Juan Munoz <juan.munoz@lemon42.com>
2017-06-04 12:22:23 -0700

    Add delete quote functionality

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,15 @@
   <div class="container">
 
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotesArray"></app-quote-grid>
-      
+    <app-quote-grid :quotes="quotesArray" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-xs12 text-center">
+        <div class="alert alert-info">
+          Info: Click in a quote to delete it.
+        </div>
+      </div>
+    </div>
+
     </div>
 </template>
 
@@ -23,6 +30,9 @@
     methods: {
       newQuote(quote) {
         this.quotesArray.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotesArray.splice(index, 1);
       }
     },
     components: {
