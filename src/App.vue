9400dbe656600659aa8789509b2e8060ba4f3ae2
Juan Munoz <juan.munoz@lemon42.com>
2017-06-04 11:48:49 -0700

    Display the quote grid and defult quote for the app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="container">
-    
+      <app-quote-grid :quotes="quotesArray"></app-quote-grid>
     </div>
 </template>
 
 <script>
-    export default {
-        
+  import QuoteGrid from './components/QuoteGrid.vue';
+
+  export default {
+    data: function() {
+      return {
+        quotesArray: [
+          'Just a Quote to see something'
+        ],
+        maxQuotes: 10
+      };
+    },
+    components: {
+      appQuoteGrid: QuoteGrid
     }
+  }
 </script>
 
 <style>
